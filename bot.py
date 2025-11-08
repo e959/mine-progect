740d235817a516704fb7fc8ef045879a7e9052ab
6a9r <59709273+e959@users.noreply.github.com>
2025-11-08 21:06:02 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands, tasks
+import os
+from keep_alive import keep_alive
+
+# تشغيل السيرفر الصغير للحفاظ على البوت Online
+keep_alive()
+
+# قراءة التوكن من متغير البيئة
+TOKEN = os.getenv("NzAwMTA3NzIwODY2Mzk4MzM5.GyT5hY.LNOxE3pAj0VbsaSeoD9SvLuzLsU_xLh4tXHVsw")
+
+# إعداد البوت
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# حدث عند تشغيل البوت
+@bot.event
+async def on_ready():
+    print(f"{bot.user} جاهز للعمل!")
+
+# مثال أمر بسيط
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
+
+# تشغيل البوت
+bot.run(TOKEN)
